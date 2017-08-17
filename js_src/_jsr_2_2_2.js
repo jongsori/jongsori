d19e0b8dcac85ce21674eec4063b78cf13731b15
sweetpea6 <sweetpea6@naver.com>
2017-08-17 17:17:33 +0900

    2-1  수정

--- js_src/_jsr_2_2_2.js
+++ js_src/_jsr_2_2_2.js
@@ -4,6 +4,7 @@
 
 $(function(){
 
+  // 마우스 오버시
   var calculateRatio = {
     $imageTop: $('.slide-wrap'),
     $imageBig: $('.slide-wrap-bigimage'),
@@ -14,20 +15,19 @@ $(function(){
     imageHeight: 0,
     areaRatio: 0,
     imageRatio: 0,
-    // 해당 부분의 값을 계산하는 메소드
-    // 마스크 영역 계산
+
     areaValue : function () {
       this.areaWidth = parseInt(this.$imageTop.css('width'));
       this.areaHeight = parseInt(this.$imageTop.css('height'));
       this.areaRatio = this.areaWidth / this.areaHeight;
     },
-    // 이미지 계산
+
     imageValue : function ($image) {
       this.imageWidth = parseInt($image.css('width'));
       this.imageHeight = parseInt($image.css('height'));
       this.imageRatio = this.imageWidth / this.imageHeight;
     },
-    // 큰 이미지에 적용
+
     applyBig : function () {
       this.areaValue();
       this.imageValue(this.$imageBig);
@@ -37,7 +37,7 @@ $(function(){
         this.$imageBig.removeClass('full-width').addClass('full-height');
       }
     },
-    // 작은 이미지에 적용
+
     applyThumb : function () {
       this.areaValue();
       for (var i = 0; i < this.$imageThumb.length; i++) {
@@ -49,16 +49,16 @@ $(function(){
         }
       }
     },
-    // 마우스 오버시 이미지 변경 함수
+
     changeImage : function( $overImage ){
       var src = $overImage.children().attr('src');
       $('.slide-wrap-bigimage').attr('src', src);
     }
   };
-  // 함수 실행
+
   calculateRatio.applyBig();
   calculateRatio.applyThumb();
-  // 이벤트 실행
+
   $('.slide-list-item').on('mouseenter', function(){
     calculateRatio.changeImage( $(this) );
     calculateRatio.applyBig();
